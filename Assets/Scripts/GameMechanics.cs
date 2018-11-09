0d0887066ef6757b57170f53199246621962bff5
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-11-08 22:08:22 -0800

    nest points in a field

--- Assets/Scripts/GameMechanics.cs
+++ Assets/Scripts/GameMechanics.cs
@@ -6,10 +6,12 @@ public class GameMechanics {
     GameState.maxSize = maxSize;
     GameState.colorIndex = new int[maxSize, maxSize];
     GameState.colorGameObject = new GameObject[maxSize, maxSize];
+    GameObject field = new GameObject ("Field");
 
     for (int x = 0; x < maxSize; x++) {
       for (int y = 0; y < maxSize; y++) {
         GameObject thisGameObject = new GameObject ("Point");
+        thisGameObject.transform.parent = field.transform;
         ObjectData thisObjectData = thisGameObject.AddComponent (typeof (ObjectData)) as ObjectData;
         thisObjectData.xIndex = x;
         thisObjectData.yIndex = y;
